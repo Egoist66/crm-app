5a9b1c4221a9ebd7c9eaaba27077ee65dda369ef
Farid Mahmudov <Razormad666@gmail.com>
2024-08-13 15:41:30 +0300

    update

--- composables/useCustomerEdit.ts
+++ composables/useCustomerEdit.ts
@@ -3,7 +3,7 @@ import { storage } from '../utils/appwrite.service';
 import type { ICustomer } from '~/types/deals.types';
 
 
-interface ICustomerFormState extends Pick<ICustomer, 'avatar_url' | 'email' | 'name' | 'from_source'> {
+export interface ICustomerFormState extends Pick<ICustomer, 'avatar_url' | 'email' | 'name' | 'from_source'> {
 
 }
 
@@ -13,24 +13,57 @@ export const useCustomerEdit = (id: string) => {
     const config = useRuntimeConfig().public
     const {handleSubmit, defineField, setValues, setFieldValue, values} = useForm<ICustomerFormState>()
 
-
-    const {data, isSuccess} = useQuery({
-        queryKey: ['get-customer', id],
-        queryFn: async () => await DB.getDocument(config.DB_ID, config.COLLECTION_CUSTOMERS, id),
-    })
-
-    watch(isSuccess, () => {
-        
-        setValues(data.value as unknown as ICustomerFormState)
-
-        
-    })
-
     const [nameField, nameFieldAttrs] = defineField('name')
     const [emailField, emailFieldAttrs] = defineField('email')
     const [fromSourceField, fromSourceFieldAttrs] = defineField('from_source')
     const [avatarField, avatarFieldAttrs] = defineField('avatar_url')
 
+    const {data, isSuccess, isLoading, isRefetching, refetch} = useQuery({
+        queryKey: ['get-customer', id],
+        refetchOnReconnect: 'always',
+        refetchOnMount: 'always',
+        gcTime: 0,
+        queryFn: async () => await DB.getDocument('crm-db', config.COLLECTION_CUSTOMERS, id),
+    })
+
+
+   watch(isSuccess,() => {
+
+    if(isSuccess.value && data.value) {
+        setValues(data.value as unknown as ICustomerFormState)
+
+    }
+
+
+   })
+
+   
+
+
+    const {mutate, isPending} = useMutation({
+        mutationKey: ['update-customer', id],
+        mutationFn: async (data: ICustomerFormState) => await DB.updateDocument(config.DB_ID, config.COLLECTION_CUSTOMERS, id, data),
+        onError: (error) => {
+
+            console.log(error)
+        },
+        onSuccess: () => {
+            refetch()
+
+        }
+    })
+
+    const onUpdate = handleSubmit((values) => {
+
+        if(!values) return
+
+        mutate({
+            avatar_url: values.avatar_url,
+            email: values.email,
+            name: values.name,
+            from_source: values.from_source
+        })
+    })
 
     return {
         nameField,
@@ -40,9 +73,16 @@ export const useCustomerEdit = (id: string) => {
         fromSourceField,
         fromSourceFieldAttrs,
         avatarField,
+        refetch,
         avatarFieldAttrs,
         handleSubmit,
         setFieldValue,
+        isLoading,
+        isRefetching,
+        isSuccess,
+        onUpdate,
+        isPending,
+        data,
         values
     }
 }
--- pages/customers/edit/[id].vue
+++ pages/customers/edit/[id].vue
@@ -1,25 +1,96 @@
-
 <script setup lang="ts">
-
 useHead({
-    title: 'RazorCRM',
-    titleTemplate: "%s - Edit customer",
+  title: "RazorCRM",
+  titleTemplate: "%s - Edit customer",
+});
+const {
+  data,
+  isLoading,
+  nameField,
+  nameFieldAttrs,
+  emailField,
+  emailFieldAttrs,
+  fromSourceField,
+  fromSourceFieldAttrs,
+  avatarField,
+  isSuccess,
+  isRefetching,
+  avatarFieldAttrs,
+  values,
+  isPending,
+  refetch,
+  onUpdate,
+} = useCustomerEdit(useRoute().params.id as string);
 
-})
-const data = useCustomerEdit(useRoute().params.id as string)
+
 
 </script>
 
+
+
+
 <template>
-    <div>
+  <div>
+    <h2 class="text-center text-2xl" v-if="isLoading || isRefetching">Loading...</h2>
+    <div v-else>
+      <h1 class="font-bold text-2xl mb-10">Editing {{ data?.name }}</h1>
 
-        {{ $route.params }}
 
+      <button
+        @click="refetch()"
+        class="btn background-[#a252c8] block mb-10 hover:text-primary"
+      >
+        Reload
+      </button>
+
+      <form v-if="isSuccess" @submit.prevent="onUpdate">
+        <UiInput
+          v-model="nameField"
+          v-bind="nameFieldAttrs"
+          placeholder="Name"
+          type="text"
+          class="input"
+        />
+
+        
+
+        <UiInput
+          v-model="emailField"
+          v-bind="emailFieldAttrs"
+          placeholder="Email"
+          type="email"
+          class="input"
+        />
+
+        <UiInput
+          v-model="fromSourceField"
+          v-bind="fromSourceFieldAttrs"
+          placeholder="Source"
+          type="text"
+          class="input"
+        />
+
+        <UiInput
+          v-model="avatarField"
+          v-bind="avatarFieldAttrs"
+          placeholder="Avatar"
+          type="text"
+          class="input"
+        />
+
+        <UiButton
+          type="submit"
+          :disabled="isPending || !nameField || !emailField"
+          class="btn background-[#a252c8] hover:text-primary"
+          >{{ isPending ? "Updating..." : "Update" }}
+        </UiButton>
+      </form>
     </div>
+  </div>
 </template>
 
-
-
 <style scoped>
-
+.input {
+  @apply border-[#161c26] mb-2 placeholder:text-[#748092] focus:border-border transition-colors;
+}
 </style>
